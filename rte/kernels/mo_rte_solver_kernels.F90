d5bc91c8311a49e61d4a6b09bb0c9dc6c13c24b6
Robert Pincus <Robert.Pincus@colorado.edu>
2020-12-21 22:39:54 -0500

    Solver indexing, actually.

--- rte/kernels/mo_rte_solver_kernels.F90
+++ rte/kernels/mo_rte_solver_kernels.F90
@@ -426,9 +426,9 @@ contains
       !
       ! Equation below is developed in Clough et al., 1992, doi:10.1029/92JD01419, Eq 13
       !
-      source_dn(icol,ilay) = (1._wp - trans(icol,ilay)) * lev_source_dn(icol,ilay) + &
+      source_dn(icol,ilay) = (1._wp - trans(icol,ilay)) * lev_source_dn(icol,ilay  ) + &
                              fact * (lev_source_dn(icol,ilay  ) - lev_source_dn(icol,ilay+1))
-      source_up(icol,ilay) = (1._wp - trans(icol,ilay)) * lev_source_up(icol,ilay  ) + &
+      source_up(icol,ilay) = (1._wp - trans(icol,ilay)) * lev_source_up(icol,ilay+1) + &
                              fact * (lev_source_up(icol,ilay+1) - lev_source_up(icol,ilay  ))
       end do
     end do
