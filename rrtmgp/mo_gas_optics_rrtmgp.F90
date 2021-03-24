e3574f1ef3f99c96fc61e4e3e52111266aa107fd
Robert Pincus <Robert.Pincus@colorado.edu>
2021-03-24 12:59:57 -0400

    OpenMP doesn't need to release the derived type

--- rrtmgp/mo_gas_optics_rrtmgp.F90
+++ rrtmgp/mo_gas_optics_rrtmgp.F90
@@ -1317,15 +1317,15 @@ contains
       !$acc           delete(this%idx_minor_upper, this%idx_minor_scaling_upper)  &
       !$acc           delete(this%kminor_start_upper, this%kminor_upper)
       !$omp target exit data map(release:this%gas_names, this%vmr_ref, this%flavor) &
-      !$omp map(release:this%gpoint_flavor, this%kmajor)  &
-      !$omp map(release:this%minor_limits_gpt_lower) &
-      !$omp map(release:this%minor_scales_with_density_lower, this%scale_by_complement_lower)  &
-      !$omp map(release:this%idx_minor_lower, this%idx_minor_scaling_lower)  &
-      !$omp map(release:this%kminor_start_lower, this%kminor_lower) &
-      !$omp map(release:this%minor_limits_gpt_upper) &
-      !$omp map(release:this%minor_scales_with_density_upper, this%scale_by_complement_upper)  &
-      !$omp map(release:this%idx_minor_upper, this%idx_minor_scaling_upper)  &
-      !$omp map(release:this%kminor_start_upper, this%kminor_upper)
+      !$omp                  map(release:this%gpoint_flavor, this%kmajor)  &
+      !$omp                  map(release:this%minor_limits_gpt_lower) &
+      !$omp                  map(release:this%minor_scales_with_density_lower, this%scale_by_complement_lower)  &
+      !$omp                  map(release:this%idx_minor_lower, this%idx_minor_scaling_lower)  &
+      !$omp                  map(release:this%kminor_start_lower, this%kminor_lower) &
+      !$omp                  map(release:this%minor_limits_gpt_upper) &
+      !$omp                  map(release:this%minor_scales_with_density_upper, this%scale_by_complement_upper)  &
+      !$omp                  map(release:this%idx_minor_upper, this%idx_minor_scaling_upper)  &
+      !$omp                  map(release:this%kminor_start_upper, this%kminor_upper)
       deallocate(this%gas_names, this%vmr_ref, this%flavor, this%gpoint_flavor, this%kmajor)
       deallocate(this%minor_limits_gpt_lower, &
                  this%minor_scales_with_density_lower, this%scale_by_complement_lower, &
@@ -1335,27 +1335,26 @@ contains
                  this%idx_minor_upper, this%idx_minor_scaling_upper, this%kminor_start_upper, this%kminor_upper)
 
       if(allocated(this%krayl)) then
-        !$acc exit data delete(this%krayl)
+        !$acc        exit data delete(     this%krayl)
         !$omp target exit data map(release:this%krayl)
         deallocate(this%krayl)
       end if
 
       if(allocated(this%planck_frac)) then
-        !$acc exit data delete(this%planck_frac, this%totplnk, this%optimal_angle_fit)
+        !$acc        exit data delete     (this%planck_frac, this%totplnk, this%optimal_angle_fit)
         !$omp target exit data map(release:this%planck_frac, this%totplnk, this%optimal_angle_fit)
         deallocate(this%planck_frac, this%totplnk, this%optimal_angle_fit)
       end if
 
       if(allocated(this%solar_source)) then
-        !$acc exit data delete(this%solar_source, this%solar_source_quiet) &
-        !$acc           delete(this%solar_source_facular,this%solar_source_sunspot)
+        !$acc exit data delete(            this%solar_source, this%solar_source_quiet) &
+        !$acc           delete(            this%solar_source_facular,this%solar_source_sunspot)
         !$omp target exit data map(release:this%solar_source, this%solar_source_quiet) &
-        !$omp map(release:this%solar_source_facular,this%solar_source_sunspot)
+        !$omp                  map(release:this%solar_source_facular,this%solar_source_sunspot)
         deallocate(this%solar_source, &
                    this%solar_source_quiet, this%solar_source_facular, this%solar_source_sunspot)
       end if
       !$acc exit data delete(this)
-      !$omp target exit data map(release:this)
     end if
 
   end subroutine finalize
