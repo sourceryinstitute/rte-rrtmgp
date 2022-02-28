319dbeac8b4bae2f8ccd2b0023fa8667965dd9ae
Brad Richardson <everythingfunctional@protonmail.com>
2022-02-28 10:42:53 -0700

    unify building documentation

--- doc/build_documentation.sh
+++ doc/build_documentation.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-rm -rf ../../public
-ford ford_site/extensions-front-matter.md 
-ford ford_site/kernels-front-matter.md 
+
+set -e
+
+rm -rf ../public
+ford ford_site/extensions-front-matter.md
+ford ford_site/kernels-front-matter.md
 cd jekyll_site/
 bundle exec jekyll build
 cd _site/
-grep -rl --include \*.html /assets/main.css | xargs sed -i 's#/rte-rrtmgp/assets/main.css#assets/main.css#g'
+if [ "$1" != "-ci" ]; then grep -rl --include \*.html /assets/main.css | xargs sed -i 's#/rte-rrtmgp/assets/main.css#assets/main.css#g'; fi
 cd ..
-cp -a _site/. ../../public
+mv _site/* ../../public
